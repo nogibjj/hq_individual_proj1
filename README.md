5a60f3e80e25c19bf8a32f51d159f0fb4450efa8
heath3rq <105904149+heath3rq@users.noreply.github.com>
2022-09-18 22:54:37 +0000

    update README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # individual_proj1
+
+In this project, I used a Kaggle dataset called Commodity Prices Dataset (2000 - 2022): https://www.kaggle.com/datasets/debashish311601/commodity-prices. The csv file includes the prices for 23 different commodities from 2000 until present. I read into the dataset using dask, and built a function to return the price of a commodity based on the commodity type and the date. 
--- commodity_dask.py
+++ commodity_dask.py
@@ -2,9 +2,8 @@ import dask.dataframe as dd
 
 
 def dask_query_commodity(location="datasets/commodity_futures.csv"):
-    """Query the commodity_futures dataset
-
-    Assumes the dataset is in the datasets folder in the root of the project
+    """Query the commodity_futures dataset. 
+       Assumes the dataset is in the datasets folder in the root of the project
     """
 
     ddf = dd.read_csv(location, blocksize=None)
